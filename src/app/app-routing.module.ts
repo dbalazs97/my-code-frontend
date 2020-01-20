933d8d4044861028f9a0293904c28a44b0033a1a
Anna Baross <baross.anna@gmail.com>
2020-01-20 22:12:22 +0100

    Create login component, route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'login',
+		loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
