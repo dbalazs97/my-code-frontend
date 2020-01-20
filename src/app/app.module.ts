a19c781d82050f8f0d13982ef2a559aa9d0e4816
Dóka Balázs <dbalazs40@gmail.com>
2020-01-20 21:51:43 +0100

    Add core and shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
 	declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
 		BrowserModule,
 		AppRoutingModule
 	],
-	providers: [],
+	providers: [
+		CoreModule,
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
